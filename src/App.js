9b40d211c70d24056d19ddc80f77c7c77e38286b
uruha <kochmusica@gmail.com>
2019-01-17 23:21:55 +0900

    add fetch get sample

--- src/App.js
+++ src/App.js
@@ -3,23 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://qiita.com/api/v2/items?page=1&per_page=20', {
+      mode: 'cors'
+    })
+      .then(res => res.json())
+      .then(json =>
+        this.setState({
+          isLoaded: true,
+          items: json
+        })
+      )
+      .catch(error =>
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      );
+  }
+
   render() {
+    const { items, isLoaded, error } = this.state;
+    let Result;
+    const Loading = <li>Loading...</li>;
+    const FailLoad = <li>{error ? error.message : ''}</li>;
+    const ItemList = items.map(item => (
+      <li key={item.id}>
+        <a href="{item.url}">{item.title}</a>
+      </li>
+    ));
+
+    if (!isLoaded) {
+      Result = Loading;
+    } else {
+      if (error) {
+        Result = FailLoad;
+      } else {
+        Result = ItemList;
+      }
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Simple React</h1>
         </header>
+        <main>
+          <ul>{Result}</ul>
+        </main>
       </div>
     );
   }
